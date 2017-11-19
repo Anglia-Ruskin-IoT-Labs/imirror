7847406385cd26afe83a9f831855616c057b435a
Attila Greguss <attila.greguss@student.anglia.ac.uk>
2017-11-19 22:27:34 +0000

    Deactivated the webbrowser

--- PIRBoot.py
+++ PIRBoot.py
@@ -4,6 +4,7 @@ import time
 import webbrowser
 import os
 import timer as countdown #methods for manipulating timer
+import threading
 
 ################################################################
 #Every Webbrowser request can be changed to an interface method#
@@ -16,39 +17,43 @@ countdown.ZeroTimer()
 
 GPIO.setup(PIR_PIN, GPIO.IN)
 
+class Run(threading.Thread, parent_func):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.start()
 
-
-print "PIR Module Startup script (CTRL+C to exit)"
-time.sleep(2)
-print "Ready"
+def start() 
+    print "PIR Module Startup script (CTRL+C to exit)"
+    time.sleep(2)
+    print "Ready"
 
 
 
 #While       
-try:
-        while True:
-                if GPIO.input(PIR_PIN) and countdown.ReadTimer() <= 0:
-                        countdown.RestartTimer()
-                        #Alexa stop
-                        #webbrowser.open('http://0.0.0.0:5005/welcome', new=0, autoraise=True) #
-                        time.sleep(1)
-                        #alexa welcome audio line?                            
-                elif GPIO.input(PIR_PIN):
-                        countdown.RestartTimer()
-                        time.sleep(1)                        
-                else:
-                        if countdown.ReadTimer() == 0:
+    try:
+            while True:
+                    if GPIO.input(PIR_PIN) and countdown.ReadTimer() <= 0:
+                            countdown.RestartTimer()
+                            #Alexa stop
+                            parent_func()
+                            time.sleep(1)
+                            #alexa welcome audio line?                            
+                    elif GPIO.input(PIR_PIN):
+                            countdown.RestartTimer()
+                            time.sleep(1)                        
+                    else:
+                            if countdown.ReadTimer() == 0:
                                 #webbrowser.open('http://0.0.0.0:5005/', new=0, autoraise=True) #black screen
                                 #stop alexa session
-                        countdown.DecrementTimer()
-                        time.sleep(1)
-                        print(countdown.ReadTimer())
+                            countdown.DecrementTimer()
+                            time.sleep(1)
+                            print(countdown.ReadTimer())
                         
 
                         
 
-except KeyboardInterrupt:
-        print'Quit'
+    except KeyboardInterrupt:
+            print'Quit'
 
 
 ####
