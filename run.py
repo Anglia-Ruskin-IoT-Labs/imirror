52fb8357b7a0c7629c36da329690005541cf64c2
Attila Greguss <floyd0122@gmail.com>
2018-08-17 15:10:06 +0100

    Fixed bug where server called from different directory and all the relative paths broke

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+import os
+import sys
+# Change working directory of this scripts directory, if it is run from somewhere else
+os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
+
 from server import flask
 # server/__init__.py runs at this import
 
